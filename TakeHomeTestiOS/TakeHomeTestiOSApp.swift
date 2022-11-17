8d15b199172434833d277f7ff31ab04575406558
Sraavan Chevireddy <63583829+SraavanChevireddy@users.noreply.github.com>
2022-11-18 00:47:13 +0530

    Initial Commit

---
+++ TakeHomeTestiOS/TakeHomeTestiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeTestiOSApp.swift
+//  TakeHomeTestiOS
+//
+//  Created by Sraavan Chevireddy on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeTestiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TakeHomeTestiOSUITests/TakeHomeTestiOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TakeHomeTestiOSUITestsLaunchTests.swift
+//  TakeHomeTestiOSUITests
+//
+//  Created by Sraavan Chevireddy on 11/18/22.
+//
+
+import XCTest
+
+final class TakeHomeTestiOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
